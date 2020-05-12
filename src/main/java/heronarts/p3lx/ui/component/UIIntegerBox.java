6548b734b020d1ae6d769b6ab146930cb2d10381
Mark Slee <mcslee@gmail.com>
2020-05-12 15:31:24 -0700

    Make discrete boxes live range-aware

--- src/main/java/heronarts/p3lx/ui/component/UIIntegerBox.java
+++ src/main/java/heronarts/p3lx/ui/component/UIIntegerBox.java
@@ -86,6 +86,20 @@ public class UIIntegerBox extends UINumberBox implements UIControlTarget {
     return this;
   }
 
+  private int getMinValue() {
+    if (this.parameter != null) {
+      return this.parameter.getMinValue();
+    }
+    return this.minValue;
+  }
+
+  private int getMaxValue() {
+    if (this.parameter != null) {
+      return this.parameter.getMaxValue();
+    }
+    return this.maxValue;
+  }
+
   public UIIntegerBox setEditMultiplier(int editMultiplier) {
     this.editMultiplier = editMultiplier;
     return this;
@@ -96,7 +110,9 @@ public class UIIntegerBox extends UINumberBox implements UIControlTarget {
     if (this.parameter != null) {
       return this.parameter.getNormalized();
     }
-    return (this.value - this.minValue) / (this.maxValue - this.minValue);
+    int min = getMinValue();
+    int max = getMaxValue();
+    return (this.value - min) / (double) (max - min);
   }
 
   public int getValue() {
@@ -117,11 +133,13 @@ public class UIIntegerBox extends UINumberBox implements UIControlTarget {
 
   protected UIIntegerBox setValue(int value, boolean pushToParameter) {
     if (this.value != value) {
-      int range = (this.maxValue - this.minValue + 1);
-      while (value < this.minValue) {
+      int min = getMinValue();
+      int max = getMaxValue();
+      int range = (max - min + 1);
+      while (value < min) {
         value += range;
       }
-      this.value = this.minValue + (value - this.minValue) % range;
+      this.value = min + (value - min) % range;
       if (this.parameter != null && pushToParameter) {
         getLX().command.perform(new LXCommand.Parameter.SetValue(this.parameter, this.value));
       }
