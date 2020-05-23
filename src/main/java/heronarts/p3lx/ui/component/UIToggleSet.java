3ef3f29c3dbcb7e85b0b19d387cd35d741f00099
Mark Slee <mcslee@gmail.com>
2020-05-23 16:45:38 -0700

    Fix UIToggleSet to work properly for DiscreteParameter with no explicit strings, whether 0-indexed or not

--- src/main/java/heronarts/p3lx/ui/component/UIToggleSet.java
+++ src/main/java/heronarts/p3lx/ui/component/UIToggleSet.java
@@ -92,10 +92,14 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
       if (this.parameter != null) {
         this.parameter.addListener(this);
         String[] options = this.parameter.getOptions();
-        if (options != null) {
-          setOptions(options);
+        if (options == null) {
+          options = new String[this.parameter.getRange()];
+          for (int i = 0; i < options.length; ++i) {
+            options[i] = Integer.toString(this.parameter.getMinValue() + i);
+          }
         }
-        setValue(this.parameter.getValuei(), false);
+        setOptions(options);
+        setValue(this.parameter.getValuei() - this.parameter.getMinValue(), false);
       }
     }
     return this;
@@ -103,7 +107,7 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
 
   public void onParameterChanged(LXParameter parameter) {
     if (parameter == this.parameter) {
-      setValue(this.parameter.getValuei(), false);
+      setValue(this.parameter.getValuei() - this.parameter.getMinValue(), false);
     }
   }
 
@@ -137,14 +141,30 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
     return this;
   }
 
+  /**
+   * Gets the index of the currently selected value in the toggle set
+   *
+   * @return currently selected index
+   */
   public int getValueIndex() {
     return this.value;
   }
 
+  /**
+   * Get the currently selected value in the toggle set
+   *
+   * @return Currently selected value
+   */
   public String getValue() {
     return this.options[this.value];
   }
 
+  /**
+   * Sets the value of the control to the given value in the toggle set
+   *
+   * @param value String value, must be one of the options in the toggle set
+   * @return this
+   */
   public UIToggleSet setValue(String value) {
     for (int i = 0; i < this.options.length; ++i) {
       if (this.options[i].equals(value)) {
@@ -162,6 +182,12 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
         + value + " " + optStr);
   }
 
+  /**
+   * Sets the value to the given index in the toggle set
+   *
+   * @param value Index in the toggle set, from 0 to range-1
+   * @return this
+   */
   public UIToggleSet setValue(int value) {
     return setValue(value, true);
   }
@@ -174,7 +200,7 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
       }
       this.value = value;
       if (this.parameter != null && pushToParameter) {
-        getLX().command.perform(new LXCommand.Parameter.SetValue(this.parameter, value));
+        getLX().command.perform(new LXCommand.Parameter.SetValue(this.parameter, this.parameter.getMinValue() + value));
       }
       onToggle(this.value);
       redraw();
@@ -208,10 +234,21 @@ public class UIToggleSet extends UIParameterComponent implements UIFocus, UICont
     }
   }
 
+  /**
+   * Invoked when value is toggled. Subclasses may override.
+   *
+   * @param value Selected index in the toggle set
+   */
   protected void onToggle(int value) {
     onToggle(getValue());
   }
 
+  /**
+   * Invoked when value is toggled, if subclass did not override the integer
+   * version of this method. Subclasses may override.
+   *
+   * @param option Selected option in the toggle set
+   */
   protected void onToggle(String option) {
   }
 
